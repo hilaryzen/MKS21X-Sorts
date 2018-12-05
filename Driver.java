4573dd4725e6c1a6aaf3ca73f403e594ed856092
Hilary Zen <hzen00@stuy.edu>
2018-12-05 01:10:19 -0500

    Edited Driver

--- Driver.java
+++ Driver.java
@@ -4,10 +4,14 @@ public class Driver {
     public static void main(String[] args) {
         int[] ary = {3, 2, 5, 6};
         Sorts.selectionSort(ary);
-        System.out.println("Sorting [3,2,5,6]: " + Arrays.toString(ary));
+        System.out.println("Sorting [3, 2, 5, 6]: " + Arrays.toString(ary));
 
         int[] ary1 = {-3, 2, 5, -6};
-        Sorts.selectionSort(ary);
-        System.out.println("Sorting [-3,2,5,-6]: " + Arrays.toString(ary1));
+        Sorts.selectionSort(ary1);
+        System.out.println("Sorting [-3, 2, 5, -6]: " + Arrays.toString(ary1));
+
+        int[] ary2 = {3, 12, 34, 6, 1000, -9};
+        Sorts.selectionSort(ary2);
+        System.out.println("Sorting [3, 12, 34, 6, 1000, -9]: " + Arrays.toString(ary2));
     }
 }
