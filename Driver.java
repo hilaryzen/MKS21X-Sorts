6422d006f9a03cfdb6b152af1b3c5dac533e6bb5
Hilary Zen <hzen00@stuy.edu>
2018-12-05 01:08:04 -0500

    Added comments and Driver

--- Driver.java
+++ Driver.java
@@ -4,6 +4,10 @@ public class Driver {
     public static void main(String[] args) {
         int[] ary = {3, 2, 5, 6};
         Sorts.selectionSort(ary);
-        System.out.println(Arrays.toString(ary));
+        System.out.println("Sorting [3,2,5,6]: " + Arrays.toString(ary));
+
+        int[] ary1 = {-3, 2, 5, -6};
+        Sorts.selectionSort(ary);
+        System.out.println("Sorting [-3,2,5,-6]: " + Arrays.toString(ary1));
     }
 }
--- Sorts.java
+++ Sorts.java
@@ -1,17 +1,21 @@
 public class Sorts {
     public static void selectionSort(int[] ary) {
+        //Store the minimum value of the array and its position
         int index;
         int min;
+        //Loops through all the elements to order them
         for (int i = 0; i < ary.length; i++) {
             index = i;
             min = ary[i];
+            //Looks for the smallest value after current position
             for (int j = i + 1; j < ary.length; j++) {
                 if (ary[j] < min) {
                     min = ary[j];
                     index = j;
                 }
-                System.out.println(min);
+                //System.out.println(min);
             }
+            //Switches values if the current one is not the minimum
             if (ary[i] != min) {
                 ary[index] = ary[i];
                 ary[i] = min;
