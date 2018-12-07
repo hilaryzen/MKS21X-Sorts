0514212459f659da41288031a48938e31cf77ab9
Hilary Zen <hzen00@stuy.edu>
2018-12-07 14:37:18 -0500

    Wrote bubbleSort

--- Sorts.java
+++ Sorts.java
@@ -27,6 +27,14 @@ public class Sorts {
     }
 
     public static void bubbleSort(int[] data) {
-      
+        for (int i = data.length; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (data[j] > data[j+1]) {
+                    int smaller = data[j+1];
+                    data[j+1] = data[j];
+                    data[j] = smaller;
+                }
+            }
+        }
     }
 }
